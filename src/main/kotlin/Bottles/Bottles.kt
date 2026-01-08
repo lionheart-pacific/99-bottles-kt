63eacfc8c7c6b80aece4e08abb6303e7611d65a2
Jaingam Piyavhatkul <jaingam@lionheartpacific.com>
2026-01-08 09:55:08 +0700

    chore: clean up code

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -1,18 +1,15 @@
 package Bottles
 
-import java.util.Locale
-import java.util.Locale.getDefault
-
 class Bottles {
     fun verse(number: Int): String {
-        fun bott(bottles:Int) = if(bottles!=1) "bottles" else "bottle"
-        fun numBott(bottles: Int) = if (bottles > 0) bottles else if (bottles == 0) "no more" else 100+bottles
+        fun bottlesOrBottle(bottles:Int) = if(bottles!=1) "bottles" else "bottle"
+        fun numberOfBottles(bottles: Int) = if (bottles > 0) bottles else if (bottles == 0) "no more" else 100+bottles
         fun takeDownOrStore(bottles: Int) = if (bottles > 1)
             "Take one down and pass it around"
         else if (bottles > 0) "Take it down and pass it around" else "Go to the store and buy some more"
         return """
-            ${numBott(number)} ${bott(number)} of beer on the wall, ${numBott(number)} ${bott(number)} of beer.
-            ${takeDownOrStore(number)}, ${numBott(number - 1)} ${bott(number - 1)} of beer on the wall.
+            ${numberOfBottles(number)} ${bottlesOrBottle(number)} of beer on the wall, ${numberOfBottles(number)} ${bottlesOrBottle(number)} of beer.
+            ${takeDownOrStore(number)}, ${numberOfBottles(number - 1)} ${bottlesOrBottle(number - 1)} of beer on the wall.
             """.trimIndent().replaceFirstChar { it.titlecase() } + "\n\n"
     }
 
@@ -25,7 +22,7 @@ class Bottles {
     }
 
     fun song(): String {
-        TODO("Don't forget about me!")
+        return verses(99, 0)
     }
 
 }
