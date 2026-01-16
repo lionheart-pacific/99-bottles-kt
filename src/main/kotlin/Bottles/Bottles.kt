88bccd40d667a2c248c6d3b51a387279fcd1cc51
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-15 16:43:43 -0800

    feat: make verse 3 test pass with if-else statement (green)

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -2,9 +2,13 @@ package Bottles
 
 class Bottles {
     fun verse(number: Int): String {
-        return """
+        if (number == 99) return """
             99 bottles of beer on the wall, 99 bottles of beer.
             Take one down and pass it around, 98 bottles of beer on the wall.
+            """.trimIndent() + "\n\n"
+        else return  """
+            3 bottles of beer on the wall, 3 bottles of beer.
+            Take one down and pass it around, 2 bottles of beer on the wall.
             """.trimIndent() + "\n\n"
     }
 
