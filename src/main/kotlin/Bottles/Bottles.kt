fba10c66fad4f7c47688eaca0cbb9dcaed58f338
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-15 16:52:22 -0800

    feat: wrap code in a new conditional to accommodate verse 2 (green)
    - choose conditional instead of generalization

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -2,6 +2,11 @@ package Bottles
 
 class Bottles {
     fun verse(number: Int): String {
+        if (number == 2) return """
+            2 bottles of beer on the wall, 2 bottles of beer.
+            Take one down and pass it around, 1 bottle of beer on the wall.
+            """.trimIndent() + "\n\n"
+
         return """
             ${number} bottles of beer on the wall, ${number} bottles of beer.
             Take one down and pass it around, ${number - 1} bottles of beer on the wall.
