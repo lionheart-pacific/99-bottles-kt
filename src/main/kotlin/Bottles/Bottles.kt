97b2b965ef324dadc31a7e61a6f5f8d401ee779d
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-15 18:30:57 -0800

    wip: add branch to switch statement to make verse 0 test pass (green)

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -3,6 +3,11 @@ package Bottles
 class Bottles {
     fun verse(number: Int): String {
         return when (number) {
+            0 -> """
+                No more bottles of beer on the wall, no more bottles of beer.
+                Go to the store and buy some more, 99 bottles of beer on the wall.
+                """.trimIndent() + "\n\n"
+
             1 -> """
                 1 bottle of beer on the wall, 1 bottle of beer.
                 Take it down and pass it around, no more bottles of beer on the wall.
