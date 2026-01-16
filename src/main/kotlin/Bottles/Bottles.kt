14c0b9092b3781d80b6aa2a51ac7bfa14dc0b149
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-15 18:25:44 -0800

    wip: use switch (Kotlin's when) to implement verse 1 (green)

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -2,15 +2,22 @@ package Bottles
 
 class Bottles {
     fun verse(number: Int): String {
-        if (number == 2) return """
-            2 bottles of beer on the wall, 2 bottles of beer.
-            Take one down and pass it around, 1 bottle of beer on the wall.
-            """.trimIndent() + "\n\n"
+        return when (number) {
+            1 -> """
+                1 bottle of beer on the wall, 1 bottle of beer.
+                Take it down and pass it around, no more bottles of beer on the wall.
+                """.trimIndent() + "\n\n"
 
-        return """
-            ${number} bottles of beer on the wall, ${number} bottles of beer.
-            Take one down and pass it around, ${number - 1} bottles of beer on the wall.
-            """.trimIndent() + "\n\n"
+            2 -> """
+                2 bottles of beer on the wall, 2 bottles of beer.
+                Take one down and pass it around, 1 bottle of beer on the wall.
+                """.trimIndent() + "\n\n"
+
+            else -> """
+                ${number} bottles of beer on the wall, ${number} bottles of beer.
+                Take one down and pass it around, ${number - 1} bottles of beer on the wall.
+                """.trimIndent() + "\n\n"
+        }
     }
 
     fun verses(upper: Int, lower: Int): String {
