a5d9ef0713a0f9970a33ee30435463cd52d32098
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-15 16:46:36 -0800

    refactor: alter if statement to return only the things that change (refactor)

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -2,13 +2,10 @@ package Bottles
 
 class Bottles {
     fun verse(number: Int): String {
-        if (number == 99) return """
-            99 bottles of beer on the wall, 99 bottles of beer.
-            Take one down and pass it around, 98 bottles of beer on the wall.
-            """.trimIndent() + "\n\n"
-        else return  """
-            3 bottles of beer on the wall, 3 bottles of beer.
-            Take one down and pass it around, 2 bottles of beer on the wall.
+        val n = if (number == 99) 99 else 3
+        return """
+            ${n} bottles of beer on the wall, ${n} bottles of beer.
+            Take one down and pass it around, ${n - 1} bottles of beer on the wall.
             """.trimIndent() + "\n\n"
     }
 
