79ce31ff70b7dddbbb296205c5056c876f6746af
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-15 16:36:43 -0800

    wip: make first test pass by hard-coding 99th verse (green)

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -2,7 +2,10 @@ package Bottles
 
 class Bottles {
     fun verse(number: Int): String {
-        TODO("Implement me!")
+        return """
+            99 bottles of beer on the wall, 99 bottles of beer.
+            Take one down and pass it around, 98 bottles of beer on the wall.
+            """.trimIndent() + "\n\n"
     }
 
     fun verses(upper: Int, lower: Int): String {
