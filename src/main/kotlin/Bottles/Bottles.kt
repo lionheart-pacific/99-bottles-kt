d8da37d484332942ad28e5416ba83a41bd797a83
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-27 20:13:25 -0800

    wip: make last lines more alike

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -5,7 +5,7 @@ class Bottles {
         return when (number) {
             0 -> """
                 ${capitalize(quantity(number))} ${container(number)} of beer on the wall, ${quantity(number)} ${container(number)} of beer.
-                ${action(number)}, 99 bottles of beer on the wall.
+                ${action(number)}, ${quantity(99)} ${container(number - 1)} of beer on the wall.
                 """.trimIndent() + "\n\n"
 
             else -> """
