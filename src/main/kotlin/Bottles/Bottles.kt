d1316eb99f95a638b50d206c8a49a93454380df3
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-27 17:30:20 -0800

    wip: replace hard-coded number in verse 1

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -9,7 +9,7 @@ class Bottles {
                 """.trimIndent() + "\n\n"
 
             1 -> """
-                1 bottle of beer on the wall, 1 bottle of beer.
+                ${number} bottle of beer on the wall, 1 bottle of beer.
                 Take it down and pass it around, no more bottles of beer on the wall.
                 """.trimIndent() + "\n\n"
 
