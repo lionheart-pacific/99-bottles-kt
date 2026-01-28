ce86df88b57e7014599b4354d75cb3ffd6b88799
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-27 18:56:11 -0800

    wip: add number argument in quantity

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -10,7 +10,7 @@ class Bottles {
 
             1 -> """
                 ${number} ${container(number)} of beer on the wall, ${number} ${container(number)} of beer.
-                Take ${pronoun(number)} down and pass it around, no more bottles of beer on the wall.
+                Take ${pronoun(number)} down and pass it around, ${quantity()} bottles of beer on the wall.
                 """.trimIndent() + "\n\n"
 
             else -> """
@@ -38,7 +38,7 @@ class Bottles {
         return "one"
     }
 
-    private fun quantity(): String {
+    private fun quantity(number: Int? = null): String {
         return "no more"
     }
 }
