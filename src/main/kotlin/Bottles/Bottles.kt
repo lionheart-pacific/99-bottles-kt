418a15454e9ad5995cc04134a5e432ed73839006
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-27 18:37:54 -0800

    wip: alter 1 case to invoke pronoun method

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -10,7 +10,7 @@ class Bottles {
 
             1 -> """
                 ${number} ${container(number)} of beer on the wall, ${number} ${container(number)} of beer.
-                Take it down and pass it around, no more bottles of beer on the wall.
+                Take ${pronoun(number)} down and pass it around, no more bottles of beer on the wall.
                 """.trimIndent() + "\n\n"
 
             else -> """
