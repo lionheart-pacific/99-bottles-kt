40bc0d5e5427695a7f69e247010307b459b0e148
Mejin Leechor <mejin@lionheartpacific.com>
2026-01-27 19:03:07 -0800

    wip: pass an argument to the quantity method from verse 1

--- src/main/kotlin/Bottles/Bottles.kt
+++ src/main/kotlin/Bottles/Bottles.kt
@@ -10,7 +10,7 @@ class Bottles {
 
             1 -> """
                 ${number} ${container(number)} of beer on the wall, ${number} ${container(number)} of beer.
-                Take ${pronoun(number)} down and pass it around, ${quantity()} bottles of beer on the wall.
+                Take ${pronoun(number)} down and pass it around, ${quantity(number - 1)} bottles of beer on the wall.
                 """.trimIndent() + "\n\n"
 
             else -> """
@@ -38,7 +38,7 @@ class Bottles {
         return "one"
     }
 
-    private fun quantity(number: Int? = 0): String {
+    private fun quantity(number: Int): String {
         if (number == 0) return "no more"
         return number.toString()
     }
