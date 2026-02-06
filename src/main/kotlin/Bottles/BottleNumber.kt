41b8b2c65afdcf04d93fcb8704238fdd3e2b5d21
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-05 19:10:55 -0800

    wip: copy primitive-obsessed methods into BottleNumber class

--- src/main/kotlin/Bottles/BottleNumber.kt
+++ src/main/kotlin/Bottles/BottleNumber.kt
@@ -1,5 +1,28 @@
 package Bottles
 
 class BottleNumber {
+    private fun quantity(number: Int): String {
+        if (number == 0) return "no more"
+        return number.toString()
+    }
 
+    private fun container(number: Int): String {
+        if (number == 1) return "bottle"
+        return "bottles"
+    }
+
+    private fun action(number: Int): String {
+        if (number == 0) return "Go to the store and buy some more"
+        return """Take ${pronoun(number)} down and pass it around"""
+    }
+
+    private fun pronoun(number: Int): String {
+        if (number == 1) return "it"
+        return "one"
+    }
+
+    private fun successor(number: Int): Int {
+        if (number == 0) return 99
+        return (number - 1)
+    }
 }
