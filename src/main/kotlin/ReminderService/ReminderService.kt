06b24c27db85fdd63302c1efb9b564097f5765f8
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-05 19:07:39 -0800

    test: add reminder service tests for shameless green practice

---
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -0,0 +1,9 @@
+package ReminderService
+
+import kotlin.time.Instant
+
+class ReminderService {
+    fun getTwoStepVerificationReminderMessage(userCreatedTime: Instant, jobExecutionTime: Instant): String? {
+        TODO("Make me shamelessly green!")
+    }
+}
