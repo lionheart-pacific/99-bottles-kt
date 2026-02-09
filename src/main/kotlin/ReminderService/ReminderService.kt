37ae6e3a620b4b73833c9ea319335a49780003f7
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-08 16:37:35 -0800

    feat: implement 6-day reminder for post-quiet period (green)

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -1,9 +1,11 @@
 package ReminderService
 
+import kotlin.time.Duration.Companion.hours
 import kotlin.time.Instant
 
 class ReminderService {
     fun getTwoStepVerificationReminderMessage(userCreatedTime: Instant, jobExecutionTime: Instant): String? {
-        return null
+        if (jobExecutionTime - userCreatedTime < 8.hours) return null
+        return "For security, you have 6 days remaining in your setup window"
     }
 }
