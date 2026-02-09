cc27e42bb1cc5e9b0fc42d9157a694f65fb04062
Saraii Pitakpalin <saraii@lionheartpacific.com>
2026-02-09 09:56:30 +0700

    test: 7-locked

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -8,6 +8,9 @@ class ReminderService {
         val dif = jobExecutionTime.minus(userCreatedTime)
         if(dif > 8.hours) {
             val remainingDays = 6 - dif.inWholeDays
+            if (remainingDays <= 0.toLong()) {
+                return "For security, your account has been locked because your 7-day setup window has expired"
+            }
             val days = if (remainingDays == 1.toLong()) "day" else "days"
             return "For security, you have $remainingDays $days remaining in your setup window"
         }
