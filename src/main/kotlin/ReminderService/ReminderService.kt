8d7284927fa0925f63d1f22dd21ce5423c6f3673
Saraii Pitakpalin <saraii@lionheartpacific.com>
2026-02-09 09:47:21 +0700

    test: 2-6days-remaining

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -1,9 +1,14 @@
 package ReminderService
 
+import kotlin.time.Duration.Companion.hours
 import kotlin.time.Instant
 
 class ReminderService {
     fun getTwoStepVerificationReminderMessage(userCreatedTime: Instant, jobExecutionTime: Instant): String? {
+        val dif = jobExecutionTime.minus(userCreatedTime)
+        if(dif > 8.hours) {
+            return "For security, you have 6 days remaining in your setup window"
+        }
         return null
     }
 }
