0f9a06dc3003458230bcd9ba4eba606d9de06405
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-08 17:40:40 -0800

    refactor: use fullDaysElapsed concept to make conditions more similar

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -11,7 +11,7 @@ class ReminderService {
 
         val fullDaysElapsed = durationSinceUserCreated.inWholeDays.toInt()
         val reportedDaysRemaining = 6 - fullDaysElapsed
-        if (reportedDaysRemaining == 1) return "For security, you have 1 day remaining in your setup window"
+        if (fullDaysElapsed == 5) return "For security, you have 1 day remaining in your setup window"
         if (fullDaysElapsed == 7) return "For security, your account has been locked because your 7-day setup window has expired"
         return "For security, you have $reportedDaysRemaining days remaining in your setup window"
     }
