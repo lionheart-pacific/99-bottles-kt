831f684d280eef0b96f807e23cf2d8153a7f0788
Leonhardt Koepsell <leonhardt@lionheartpacific.com>
2026-02-09 10:06:22 +0700

    refactor: use when block for branching

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -8,10 +8,12 @@ class ReminderService {
         val durationSinceUserCreated = jobExecutionTime - userCreatedTime
         val wholeDaysSinceUserCreated = durationSinceUserCreated.inWholeDays.toInt()
         val reportedDaysRemaining = 6 - wholeDaysSinceUserCreated
-        if (durationSinceUserCreated < 8.hours) return null
-        if (wholeDaysSinceUserCreated == 5) return "For security, you have $reportedDaysRemaining day remaining in your setup window"
-        if (wholeDaysSinceUserCreated < 7) return "For security, you have $reportedDaysRemaining days remaining in your setup window"
-        if (wholeDaysSinceUserCreated == 7) return "For security, your account has been locked because your 7-day setup window has expired"
-        return null
+        return when {
+            durationSinceUserCreated < 8.hours -> null
+            wholeDaysSinceUserCreated == 5 -> "For security, you have $reportedDaysRemaining day remaining in your setup window"
+            wholeDaysSinceUserCreated < 7 -> "For security, you have $reportedDaysRemaining days remaining in your setup window"
+            wholeDaysSinceUserCreated == 7 -> "For security, your account has been locked because your 7-day setup window has expired"
+            else -> null
+        }
     }
 }
