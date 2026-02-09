243f14f7882822c683b9a6ff5e1d843f4a3b308d
Leonhardt Koepsell <leonhardt@lionheartpacific.com>
2026-02-09 10:03:48 +0700

    refactor: compute branches based on whole days since user created

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -1,6 +1,5 @@
 package ReminderService
 
-import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Instant
 
@@ -10,8 +9,8 @@ class ReminderService {
         val wholeDaysSinceUserCreated = durationSinceUserCreated.inWholeDays.toInt()
         val reportedDaysRemaining = 6 - wholeDaysSinceUserCreated
         if (durationSinceUserCreated < 8.hours) return null
-        if (reportedDaysRemaining == 1) return "For security, you have $reportedDaysRemaining day remaining in your setup window"
-        if (reportedDaysRemaining > 0) return "For security, you have $reportedDaysRemaining days remaining in your setup window"
+        if (wholeDaysSinceUserCreated == 5) return "For security, you have $reportedDaysRemaining day remaining in your setup window"
+        if (wholeDaysSinceUserCreated < 7) return "For security, you have $reportedDaysRemaining days remaining in your setup window"
         if (wholeDaysSinceUserCreated == 7) return "For security, your account has been locked because your 7-day setup window has expired"
         return null
     }
