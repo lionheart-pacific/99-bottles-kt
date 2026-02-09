a826c24f679e9b3c4b3eba4032df0211199ba6c2
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-08 17:42:59 -0800

    refactor: replace if statements with switch/when

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -11,8 +11,11 @@ class ReminderService {
 
         val fullDaysElapsed = durationSinceUserCreated.inWholeDays.toInt()
         val reportedDaysRemaining = 6 - fullDaysElapsed
-        if (fullDaysElapsed == 5) return "For security, you have 1 day remaining in your setup window"
-        if (fullDaysElapsed == 7) return "For security, your account has been locked because your 7-day setup window has expired"
-        return "For security, you have $reportedDaysRemaining days remaining in your setup window"
+
+        return when (fullDaysElapsed) {
+            5 -> "For security, you have 1 day remaining in your setup window"
+            7 -> "For security, your account has been locked because your 7-day setup window has expired"
+            else -> "For security, you have $reportedDaysRemaining days remaining in your setup window"
+        }
     }
 }
