9d1a9b033ab580f6bc5b33dc4d46946ca2a307b6
Saraii Pitakpalin <saraii@lionheartpacific.com>
2026-02-09 09:37:04 +0700

    test: 1-no-reminder

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -4,6 +4,6 @@ import kotlin.time.Instant
 
 class ReminderService {
     fun getTwoStepVerificationReminderMessage(userCreatedTime: Instant, jobExecutionTime: Instant): String? {
-        TODO("Make me shamelessly green!")
+        return null
     }
 }
