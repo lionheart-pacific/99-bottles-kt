ea09467bd78fc6abcb1abfe2bcf7d2c35cf9de91
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-08 17:38:08 -0800

    feat: enable account locked notice at 7 days (green)

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -1,5 +1,6 @@
 package ReminderService
 
+import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Instant
 
@@ -11,6 +12,7 @@ class ReminderService {
 
         val reportedDaysRemaining = 6 - durationSinceUserCreated.inWholeDays.toInt()
         if (reportedDaysRemaining == 1) return "For security, you have 1 day remaining in your setup window"
+        if (durationSinceUserCreated.inWholeDays.toInt() == 7) return "For security, your account has been locked because your 7-day setup window has expired"
         return "For security, you have $reportedDaysRemaining days remaining in your setup window"
     }
 }
