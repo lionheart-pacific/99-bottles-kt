17ed1b8e8da07859d40f343f5aa71453ef30e4c7
Mejin Leechor <mejin@lionheartpacific.com>
2026-02-08 17:27:41 -0800

    feat: implement 1-day reminder by hardcoding (green)
    - this is the shameless green move, where we resist premature abstraction

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -6,8 +6,11 @@ import kotlin.time.Instant
 class ReminderService {
     fun getTwoStepVerificationReminderMessage(userCreatedTime: Instant, jobExecutionTime: Instant): String? {
         val durationSinceUserCreated = jobExecutionTime - userCreatedTime
+
         if (durationSinceUserCreated < 8.hours) return null
-        val reportedDaysRemaining = 6 - durationSinceUserCreated.inWholeDays
+
+        val reportedDaysRemaining = 6 - durationSinceUserCreated.inWholeDays.toInt()
+        if (reportedDaysRemaining == 1) return "For security, you have 1 day remaining in your setup window"
         return "For security, you have $reportedDaysRemaining days remaining in your setup window"
     }
 }
