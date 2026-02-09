a8a692994c646c5c2bdbe32cb1c52bf4a4d24407
Leonhardt Koepsell <leonhardt@lionheartpacific.com>
2026-02-09 10:00:29 +0700

    feat: report reminder when 8 days remaining and user created during quiet period

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -1,5 +1,6 @@
 package ReminderService
 
+import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Instant
 
@@ -11,6 +12,7 @@ class ReminderService {
         if (durationSinceUserCreated < 8.hours) return null
         if (reportedDaysRemaining == 1) return "For security, you have $reportedDaysRemaining day remaining in your setup window"
         if (reportedDaysRemaining > 0) return "For security, you have $reportedDaysRemaining days remaining in your setup window"
-        return "For security, your account has been locked because your 7-day setup window has expired"
+        if (wholeDaysSinceUserCreated == 7) return "For security, your account has been locked because your 7-day setup window has expired"
+        return null
     }
 }
