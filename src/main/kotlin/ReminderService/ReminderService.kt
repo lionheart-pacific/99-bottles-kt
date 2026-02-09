0e1bfbc28392ec2683cb64f9cbaa1fd89d8f9a23
Leonhardt Koepsell <leonhardt@lionheartpacific.com>
2026-02-09 09:54:00 +0700

    refactor: reorder logical branches to make them more logically sequential

--- src/main/kotlin/ReminderService/ReminderService.kt
+++ src/main/kotlin/ReminderService/ReminderService.kt
@@ -10,7 +10,7 @@ class ReminderService {
         val reportedDaysRemaining = 6 - wholeDaysSinceUserCreated
         if (durationSinceUserCreated < 8.hours) return null
         if (reportedDaysRemaining == 1) return "For security, you have $reportedDaysRemaining day remaining in your setup window"
-        if (reportedDaysRemaining <= 0) return "For security, your account has been locked because your 7-day setup window has expired"
-        return "For security, you have $reportedDaysRemaining days remaining in your setup window"
+        if (reportedDaysRemaining > 0) return "For security, you have $reportedDaysRemaining days remaining in your setup window"
+        return "For security, your account has been locked because your 7-day setup window has expired"
     }
 }
